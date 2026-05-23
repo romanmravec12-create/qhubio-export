d56cef1f6d99db9130b076e15b61f51ddc967942
romanmravec12-create <roman.mravec12@gmail.com>
2026-05-23 18:14:32 +0200

    Update export.js

--- api/export.js
+++ api/export.js
@@ -65,7 +65,10 @@ export default async function handler(req, res) {
   // ✅ CORS
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader(
+  "Access-Control-Allow-Headers",
+  "Content-Type, Authorization, apikey"
+);
 
   if (req.method === "OPTIONS") {
     return res.status(200).end();
